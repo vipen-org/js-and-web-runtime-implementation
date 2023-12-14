198f08e452a755432b95230ea71f0d229d4b3507
dasdeo <dasdeo@proton.me>
2023-12-14 13:55:52 +0100

    Update createDefaultContext.

--- src/shared/createDefaultContext.mjs
+++ src/shared/createDefaultContext.mjs
@@ -1,7 +1,15 @@
+function logLine(line) {
+	if (typeof process === "object") {
+		process.stderr.write(`${line}\n`)
+	} else if (typeof console === "object") {
+		console.log(line)
+	}
+}
+
 export default function createDefaultContext() {
 	return {
-		log() {
-			
+		log(message) {
+			logLine(`createDefaultContext.log(${message})`)
 		}
 	}
 }
