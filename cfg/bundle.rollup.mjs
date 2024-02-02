edca84fd6b226bedb02349591c9b69e8972cfd51
dasdeo <mark@anio.software>
2024-02-02 15:05:46 +0100

    Use @rollup/plugin-node-resolve.

--- cfg/bundle.rollup.mjs
+++ cfg/bundle.rollup.mjs
@@ -1,8 +1,12 @@
+import resolve from "@rollup/plugin-node-resolve"
+
 export default {
 	input: "./src/code_for_bundled_version/index.mjs",
 
 	output: {
 		file: "./dist/virtual.mjs",
 		format: "es"
-	}
+	},
+
+	plugins: [resolve()]
 }
--- cfg/node.rollup.mjs
+++ cfg/node.rollup.mjs
@@ -1,8 +1,12 @@
+import resolve from "@rollup/plugin-node-resolve"
+
 export default {
 	input: "./src/code_for_live_node_version/index.mjs",
 
 	output: {
 		file: "./dist/node.mjs",
 		format: "es"
-	}
+	},
+
+	plugins: [resolve()]
 }
